ba3a2a11802a7165be5787ad6cfe5acf42b4b842
Stéphane Graber <stgraber@ubuntu.com>
2014-01-06 15:50:37 -0500

    Really fix the tests...

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@ teststring="blah blah blah"
 
 for pastebin in $(./pastebinit -l | egrep "^-" | sed "s/^- //g")
 do
-    echo "Trying http://$pastebin ($interpreter)"
-    URL=$(echo -e "$teststring\n$teststring\n$teststring" | $interpreter pastebinit -b http://$pastebin)
+    echo "Trying http://$pastebin"
+    URL=$(echo -e "$teststring\n$teststring\n$teststring" | ./pastebinit -b http://$pastebin)
     out=$(wget -O - -q "$URL" | grep "$teststring")
     if [ -n "$out" ]; then
-        echo "PASS: http://$pastebin ($URL) ($interpreter)"
+        echo "PASS: http://$pastebin ($URL)"
     else
-        echo "FAIL: http://$pastebin ($URL) ($interpreter)"
+        echo "FAIL: http://$pastebin ($URL)"
     fi
     echo ""
 done
