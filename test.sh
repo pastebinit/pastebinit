bee39c8693e2d23cd5524568844428ff255273b7
Alberto Fuentes <pajaro@gmail.com>
2016-02-29 11:16:09 -0500

    Don't pass the URL scheme when not needed.

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ teststring="test from pastebinit"
 for interpreter in python python3; do
     for pastebin in $(ls pastebin.d/*.conf | sed 's~^pastebin.d/\(.*\)\.conf$~\1~')
     do
-        echo "Trying http://$pastebin ($interpreter)"
-        URL=$(echo "$teststring\n$teststring\n$teststring" | $interpreter ./pastebinit -b http://$pastebin)
+        echo "Trying $pastebin ($interpreter)"
+        URL=$(echo "$teststring\n$teststring\n$teststring" | $interpreter ./pastebinit -b $pastebin)
 
         if [ "$pastebin" = "paste.ubuntu.org.cn" ]; then
             out=$(wget -O - -q "$URL" | gzip -d | grep "$teststring")
@@ -14,9 +14,9 @@ for interpreter in python python3; do
         fi
 
         if [ -n "$out" ]; then
-            echo "PASS: http://$pastebin ($URL) ($interpreter)"
+            echo "PASS: $pastebin ($URL) ($interpreter)"
         else
-            echo "FAIL: http://$pastebin ($URL) ($interpreter)"
+            echo "FAIL: $pastebin ($URL) ($interpreter)"
         fi
         echo ""
     done
